760ba7bdc64f684db1d207489050c0d91f254919
ClaudomiroSales <claudomiro.sales@gmail.com>
2023-11-21 07:39:06 -0300

    Testando Stories

---
+++ main.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using std::cout;
+
+#include "../include/Stories.h"
+
+int main()
+{
+
+  Stories stories;
+
+  cout << "Teste Stories.\n";
+  cout << stories;
+
+  return 0;
+}
